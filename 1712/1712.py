409830b8b35063cbc61d696a0669345cb33fffc5
unknown <tega1996@naver.com>
2021-09-04 01:50:05 +0900

    1712 시간초과 해결하는 답

--- 1712/1712.py
+++ 1712/1712.py
@@ -1,13 +1,6 @@
-a = list(map(int,input().split()))
-cnt = 1
-while cnt:
-    cost = a[0] + cnt *a[1]
-    price = a[2]
-    if price * cnt - cost >0:
-        break
-    elif price < a[1]:
-        cnt = -1
-        break
-    else:
-        cnt+=1
-print(cnt)
+A, B, C = map(int,input().split())
+
+if B>=C:
+    print(-1)
+else:
+    print(int(A/(C-B)+1))
