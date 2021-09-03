01211277ebb510638d031cfaa09016ae68cbf3ae
unknown <tega1996@naver.com>
2021-09-03 23:37:27 +0900

    2641번 풀이

--- 2941/2941.py
+++ 2941/2941.py
@@ -1,11 +1,10 @@
-a = list(map(str,input().split()))
-print(a)
+a = input()
+#print(a)
 croa_list = ["c=","c-","dz=","d-","lj","nj","s=","z="]
-print(croa_list)
+#print(croa_list)
 cnt = 0
-count = 0
 for i in croa_list:
-    for j in a:
-        if i in j:
-            cnt+=1
-print(cnt)
+    index = a.count(i)
+    cnt+=index
+count = len(a) - cnt*2
+print(count+cnt)
