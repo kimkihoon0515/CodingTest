0657bc9cb4def39cc8314807471e3b615fa1548a
unknown <tega1996@naver.com>
2021-09-06 23:50:42 +0900

    1929번 풀이

--- 1929/1929.py
+++ 1929/1929.py
@@ -1,1 +1,13 @@
 m, n = map(int,input().split())
+
+def is_prime_number(x): # 소수를 판별하는 알고리즘
+    if x == 1: 
+        return False
+    for i in range(2,int(x**0.5)+1):
+        if x % i == 0:
+            return False
+    return True
+
+for i in range(m,n+1):
+    if is_prime_number(i) == True:
+        print(i)
