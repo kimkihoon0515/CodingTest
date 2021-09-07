cf3143440b3a246b61499ad56be16f1acfb0e4c5
unknown <tega1996@naver.com>
2021-09-08 00:04:13 +0900

    3009번 풀이

--- 1085/1085.py
+++ 1085/1085.py
@@ -1,16 +1,16 @@
 x,y,w,h = map(int,input().split()) # x y w h 를 입력받는다.
 
-x_distance = abs(x-w)
-y_distance = abs(y-h)
+x_distance = abs(x-w) # x축과의 거리의 절대값을 구한다.
+y_distance = abs(y-h) # y축과의 거리의 절대값을 구한다.
 
-min_x = 0
-min_y = 0
+min_x = 0 # x축상에서의 거리중 짧은 것
+min_y = 0 # y축상에서의 거리중 짧은 것
 
-if x_distance < x :
+if x_distance < x : # x축과의 거리중 짧은 것을 min 값으로 둔다.
     min_x = x_distance
 else:
     min_x = x
-if y_distance < y :
+if y_distance < y : # y축과의 거리중 짧은 것을 min 값으로 둔다.
     min_y = y_distance
 else:
     min_y = y
