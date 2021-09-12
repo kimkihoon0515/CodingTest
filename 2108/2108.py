08663e3a27b5ad73bf1775aa4aa1e7e18eb13559
unknown <tega1996@naver.com>
2021-09-12 23:23:58 +0900

    1427풀이

--- 2108/2108.py
+++ 2108/2108.py
@@ -1,20 +1,20 @@
-from collections import Counter
-n = int(input()) 
+from collections import Counter # 최빈값을 구하기 위해서 import 
+n = int(input())  # 범위 설정해주는 변수
 
-list = []
+list = [] 
 sum = 0
-for i in range(n):
+for i in range(n): # n 개만큼 입력받아서 list에 저장
     list.append(int(input()))
 
 for i in range(len(list)):
     sum += list[i]
-avg = round(sum/len(list))
+avg = round(sum/len(list)) # 평균을 구해준다.
 
 print(avg)
-list.sort()
+list.sort() # 정렬 후 중앙값 출력
 print(list[n//2])
 
-cnt = Counter(list).most_common()
+cnt = Counter(list).most_common() # 최빈값 출력을 위한 알고리즘
 if len(cnt) == 1:
     print(cnt[0][0])
 else:
@@ -23,5 +23,5 @@ else:
     else:
         print(cnt[0][0])
 
-print(list[len(list)-1]-list[0])
+print(list[len(list)-1]-list[0]) # 범위 출력
 
