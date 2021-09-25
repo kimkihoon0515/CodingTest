0aeeec636c71e1a7cd29fd2591c426210629b6a0
unknown <tega1996@naver.com>
2021-09-25 20:04:40 +0900

    15654번 풀이

--- 15654/15654.py
+++ 15654/15654.py
@@ -1,11 +1,19 @@
-from itertools import permutations 
-n,m = map(int,input().split()) # n,m을 입력받는다.
+n,m = map(int,input().split()) # n,m 을 입력받는다.
 
-num = sorted(list(map(int,input().split()))) # 입력받은 숫자들을 배열에 sort해서 저장한다.
+list = sorted(map(int,input().split())) # 숫자들을 입력받아서 배열에 정렬하여 저장한다.
 
-com = []
+visited = [False] * n # 탐색 확인용 배열
+out = [] # 출력용 배열
 
-com.extend(list(map(' '.join,permutations(map(str,num),m)))) # 출력형태에 맞게 com 이라는 배열에 permutation을 이용해서 저장한다.
-
-for i in range(len(com)): # 조건에 맞게 출력한다.
-    print(com[i])
+def dfs(depth,n,m): # dfs 알고리즘 구조 
+    if depth == m: # m개의 숫자길이가 되면 형식에 맞게 출력한다.
+        print(' '.join(map(str,out)))
+        return
+    for i in range(n): # 배열의 길이가 m개가 아니면 n번만큼 반복문을 돌린다.
+        if not visited[i]: # 아직 탐색을 하지 않았다면
+            visited[i] = True # 탐색을 했다고 표시하고 list[i]를 출력용 배열에 입력한다.
+            out.append(list[i])
+            dfs(depth+1,n,m) # 재귀 방식으로 다시 호출한다.
+            out.pop() 
+            visited[i] = False
+dfs(0,n,m)
