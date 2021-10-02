bc60b146bf0c07eb9946314c037305c8657be680
unknown <tega1996@naver.com>
2021-10-02 16:01:36 +0900

    12865번 풀이

--- 1912/1912.py
+++ 1912/1912.py
@@ -1,9 +1,9 @@
-n = int(input())
+n = int(input()) # 숫자의 개수를 입력받는다.
 
-li = list(map(int,input().split()))
-result=[]
-result.append(li[0])
-for i in range(n-1):
-    result.append(max(result[i]+li[i+1],li[i+1]))
+li = list(map(int,input().split())) # 숫자들을 배열에 넣어 저장한다.
+result=[] # 합의 최대값들을 넣을 배열
+result.append(li[0]) # 첫 번째는 그냥 넣어준다. 
+for i in range(n-1): # 2번째부터는 새로 들어온 숫자와 그 이전까지의 배열합의 최대값을 비교하여 더 큰쪽을 남겨둔다.
+    result.append(max(result[i]+li[i+1],li[i+1])) 
 
-print(max(result))
+print(max(result)) # 최대값을 출력한다.
