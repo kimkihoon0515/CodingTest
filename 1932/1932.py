a2db3bbe16f69a92c6b78d5ee972c39f21f0aae1
unknown <tega1996@naver.com>
2021-10-07 16:47:47 +0900

    1932번 풀이

--- 1932/1932.py
+++ 1932/1932.py
@@ -1,3 +1,4 @@
+""" 시간초과 오류 발생 
 n = int(input())
 li=[]
 for _ in range(n):
@@ -13,3 +14,38 @@ for i in range(1,n):
             li[i][j] = max(li[i-1][j-1],li[i-1][j]) + li[i][j]
 
 print(max(li[n-1]))
+"""
+
+import sys
+from collections import deque
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+    p = sys.stdin.readline()
+    n = int(sys.stdin.readline())
+    a = sys.stdin.readline().rstrip()[1:-1].split(',')
+    stack = deque(a)
+    reverse_flag = 0 # R이 짝수번 나오는지 홀수번 나오는지 검사하기위한 변수
+    error_flag = 0 # error 가 나오는지 저장하기 위한 변수
+
+    if n == 0:
+        stack = [] 
+    for i in p:
+        if i == 'R':
+            reverse_flag += 1
+        if i == 'D':
+            if len(stack) < 1:
+                error_flag = 1
+                print('error')
+                break
+            else:
+                if reverse_flag % 2 == 0:
+                    stack.popleft()
+                else:
+                    stack.pop()
+    if error_flag == 0:
+        if reverse_flag % 2 == 0:
+            print('[' + ','.join(stack)+']')
+        else:
+            stack.reverse()
+            print('['+','.join(stack)+']')
