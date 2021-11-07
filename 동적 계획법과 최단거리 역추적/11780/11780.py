854ce03cf97293c73e60eca019edb07f704dab29
unknown <tega1996@naver.com>
2021-11-07 23:17:29 +0900

    11780번 풀이

---
+++ 동적 계획법과 최단거리 역추적/11780/11780.py
@@ -0,0 +1,31 @@
+import sys
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+inf = sys.maxsize
+graph = [[[] for _ in range(n+1)] for _ in range(n+1)]
+dp = [[inf] * (n+1) for _ in range(n+1)]
+
+for _ in range(m):
+	a, b, c = map(int,sys.stdin.readline().split())
+	dp[a][b] = min(dp[a][b], c)
+	graph[a][b] = [a, b]
+ 
+for k in range(1, n+1): # floyd warshall 알고리즘
+	for i in range(1, n+1):
+		for j in range(1, n+1):
+			if i != j:
+				if dp[i][j] > dp[i][k] + dp[k][j]:
+					dp[i][j] = dp[i][k] + dp[k][j]
+					graph[i][j] = graph[i][k][:-1] + graph[k][j]
+ 
+for i in dp[1:]:
+	for j in i[1:]:
+		print(0 if j == inf else j, end = ' ')
+	print()
+
+for i in range(1, n+1):
+	for j in range(1, n+1):
+		if dp[i][j] == inf or i == j:
+			print(0)
+		else:
+			print(len(graph[i][j]), *graph[i][j])
