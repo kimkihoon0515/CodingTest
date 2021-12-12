43b029eef5670ebee811a13d78617c33f83fe2b4
unknown <tega1996@naver.com>
2021-12-12 20:38:31 +0900

    10546번 수정

--- 해시를 이용한 집합과 맵/10546/10546.py
+++ 해시를 이용한 집합과 맵/10546/10546.py
@@ -8,13 +8,14 @@ name = [sys.stdin.readline().strip() for _ in range(n)]
 complete = [sys.stdin.readline().strip() for _ in range(n-1)]
 
 n = {}
-c = {}
+
 for i in name:
     n[i] = n.get(i,0)+1
 
 for i in complete:
     n[i] -= 1
     
-print(n)
+ans = [k for k,v in n.items() if v ==1]
+print(ans[0])
 
 
