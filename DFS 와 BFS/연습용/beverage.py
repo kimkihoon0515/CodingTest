960af295434001689f2a27ada18732bfc3f536e9
unknown <tega1996@naver.com>
2021-12-17 00:15:38 +0900

    음료수문제 풀이

--- DFS 와 BFS/연습용/beverage.py
+++ DFS 와 BFS/연습용/beverage.py
@@ -1,27 +1,35 @@
 import sys
+from collections import deque
 
 n,m = map(int,sys.stdin.readline().split())
-li = []
 
-for _ in range(n):
-    li.append(list(map(int,input())))
+graph = [list(map(int,sys.stdin.readline().strip())) for _ in range(n)]
 
-def dfs(x,y):
-    if x <0 or y < 0 or x >=n or y >= m:
-        return False
-    if li[x][y] == 0:
-        li[x][y] = 1
-        dfs(x-1,y)
-        dfs(x,y-1)
-        dfs(x+1,y)
-        dfs(x,y+1)
-        return True
-    return False
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
-result = 0
+def bfs(a,b):
+    queue = deque()
+    visit= [[0 for i in range(m)] for _ in range(n)]
+    queue.append([a,b])
+    visit[a][b] = 1
+    
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 <= nx < n and 0 <= ny < m and visit[nx][ny] == 0:
+                if graph[nx][ny] == 0:
+                    graph[nx][ny] = 1
+                    queue.append([nx,ny])
+                    visit[nx][ny] = 1
 
+cnt = 0
 for i in range(n):
     for j in range(m):
-        if dfs(i,j) == True:
-            result +=1
-print(result)
+        if graph[i][j] == 0:
+            bfs(i,j)
+            cnt +=1
+print(cnt)
