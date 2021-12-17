861a01a0e8a32b5ff29f403b8f062a666f906059
unknown <tega1996@naver.com>
2021-12-17 14:55:44 +0900

    15565번 수정

--- 투 포인터/15565/15565.py
+++ 투 포인터/15565/15565.py
@@ -4,23 +4,19 @@ n,k = map(int,sys.stdin.readline().split())
 
 doll = list(map(int,sys.stdin.readline().split()))
 
-result = len(doll)
 
 if doll.count(1) < k:
     print(-1)
     exit()
 else:
-    for start in range(len(doll)):
-        end = start
-        cnt = 0
-        while end < len(doll) :
-            if doll[end] == 1:
-                cnt +=1
-            if cnt == k:
-                result = min(result,end-start+1)
-                break
-            end +=1
+    lion = [k for k,v in enumerate (doll) if v == 1]
+    result = sys.maxsize
+    start = 0
+    end = k-1
+    while True:
+        result = min(result,lion[end]-lion[start]+1)
+        if end == len(lion)-1:
+            break
+        start+=1
+        end +=1
 print(result)
-
-        
-                 
