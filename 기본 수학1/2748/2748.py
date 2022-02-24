26279f23d678c0445bc6ba6dac03895165bb0acf
kihoon kim <tega1996@naver.com>
2022-02-24 21:32:35 +0900

    2748번 풀이

---
+++ 기본 수학1/2748/2748.py
@@ -0,0 +1,18 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+fib = []
+
+ans = 0
+
+for i in range(n+1):
+    if i ==0:
+        ans = 0
+    elif i <= 2:
+        ans = 1
+        
+    else:
+        ans = fib[-1]+ fib[-2]
+    fib.append(ans)
+print(fib)
