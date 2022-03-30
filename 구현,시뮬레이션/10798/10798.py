e55704f57ade77587fe67e87427fd576e21a2cb1
kimkihoon0515 <tega1996@naver.com>
2022-03-30 23:41:55 +0900

    10798번 풀이

---
+++ 구현,시뮬레이션/10798/10798.py
@@ -0,0 +1,14 @@
+words = []
+length = []
+for _ in range(5):
+    word = input()
+    words.append(word)
+    length.append(len(word))
+    
+result = ''
+for i in range(max(length)):
+    for j in range(5):
+        if i < length[j]:
+            result += words[j][i]
+    
+print(result)
