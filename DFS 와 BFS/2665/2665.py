8fbeff689e0577af643c070e3345ee191a8f2819
kimkihoon0515 <tega1996@naver.com>
2022-04-29 22:44:05 +0900

    백준 2665번 풀이

---
+++ DFS 와 BFS/2665/2665.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+a = [list(map(int,sys.stdin.readline().strip())) for i in range(n)]
+visit = [[0 for i in range(n)] for j in range(n)]
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs():
+    queue = deque()
+    queue.append([0,0])
+    visit[0][0] = 1
+    
+    while queue:
+        x,y = queue.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 <= nx < n and 0 <= ny < n:
+                if visit[nx][ny] == 0:
+                    if a[nx][ny] == 0:
+                        visit[nx][ny]  = visit[x][y] + 1
+                        queue.append([nx,ny])
+                    else:
+                        visit[nx][ny] = visit[x][y]
+                        queue.appendleft([nx,ny])
+bfs()
+print(visit[n-1][n-1]-1) # 시작 지점을 1로 계산했으므로 마지막에 1을 빼준다.
