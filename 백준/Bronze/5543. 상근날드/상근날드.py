15d75ebfcc98f8928dd9c5e3e79168cc51c12af7
kimkihoon <63439911+kimkihoon0515@users.noreply.github.com>
2022-06-03 20:22:17 +0900

    [Bronze IV] Title: 상근날드, Time: 108 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.py
@@ -0,0 +1,17 @@
+'''
+상근날드에서 가장 잘 팔리는 메뉴는 세트 메뉴이다. 주문할 때, 자신이 원하는 햄버거와 음료를 하나씩 골라, 세트로 구매하면, 가격의 합계에서 50원을 뺀 가격이 세트 메뉴의 가격이 된다.
+
+햄버거는 총 3종류 상덕버거, 중덕버거, 하덕버거가 있고, 음료는 콜라와 사이다 두 종류가 있다.
+
+햄버거와 음료의 가격이 주어졌을 때, 가장 싼 세트 메뉴의 가격을 출력하는 프로그램을 작성하시오.
+'''
+
+import sys
+
+burgers = sorted([int(sys.stdin.readline()) for _ in range(3)])
+coke = sorted([int(sys.stdin.readline().strip()) for _ in range(2)])
+
+set = (burgers[0]+coke[0])-50
+
+print(set)
+
