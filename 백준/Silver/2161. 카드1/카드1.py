5a8dd625b2d536d185e25ed8df65798ce1293e83
kimkihoon <63439911+kimkihoon0515@users.noreply.github.com>
2022-06-30 22:51:12 +0900

    [Silver V] Title: 카드1, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -0,0 +1,10 @@
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+card = [i for i in range(1,n+1)]
+trash = []
+while len(card)>1:
+    trash.append(card.pop(0))
+    card.append(card.pop(0))
+print(*trash,card[-1])
