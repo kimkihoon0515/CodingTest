7de05a0982d0a670db279891968ac131714c4887
kimkihoon <63439911+kimkihoon0515@users.noreply.github.com>
2022-08-05 18:09:08 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+# 아직 입양을 못 간 동물 중, 가장 오래 보호소에 있었던 동물 3마리의 이름과 보호 시작일을 조회하는 SQL문을 작성해주세요. 이때 결과는 보호 시작일 순으로 조회해야 합니다.
+SELECT ANIMAL_INS.NAME,ANIMAL_INS.DATETIME
+FROM ANIMAL_INS left JOIN ANIMAL_OUTS
+ON ANIMAL_INS.ANIMAL_ID = ANIMAL_OUTS.ANIMAL_ID
+WHERE ANIMAL_OUTS.DATETIME is NULL
+ORDER BY ANIMAL_INS.DATETIME
+LIMIT 3
