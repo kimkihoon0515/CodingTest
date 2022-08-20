591004baad6a4385aad465f1421aefd9f2dab543
Soarin Kim <soarin@madsq.net>
2022-08-21 02:57:28 +0900

    실전 모의고사 2번 풀이

---
+++ 프로그래머스/실전모의고사2회/2번.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+def solution(topping):
+    answer = 0
+
+    hash = Counter(topping)
+    
+    dic = {}
+
+    for i in range(len(topping)):
+        if topping[i] in hash and hash[topping[i]]>0:
+            hash[topping[i]] -= 1
+            if hash[topping[i]] == 0:
+                del hash[topping[i]]
+            if topping[i] not in dic:
+                dic[topping[i]] = 1
+            else:
+                dic[topping[i]]+=1
+        
+        if len(hash) == len(dic):
+            answer+=1
+
+
+    return answer
