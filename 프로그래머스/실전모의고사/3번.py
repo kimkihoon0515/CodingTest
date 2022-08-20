e64fc435839c0f91d6ccb035523c95e2df65ae8b
Soarin Kim <soarin@madsq.net>
2022-08-20 16:54:40 +0900

    3번 풀이

---
+++ 프로그래머스/실전모의고사/3번.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def solution(order):
+    answer = 0
+
+    main_belt = deque([i for i in range(1,len(order)+1)])
+    sub_belt = []
+
+    for i in range(len(order)):
+        if sub_belt and sub_belt[-1] == order[i]:
+            sub_belt.pop()
+            answer+=1
+        else:
+            if not main_belt:
+                break
+            while main_belt:
+                box = main_belt.popleft()
+                if box==order[i]:
+                    answer+=1
+                    break
+                else:
+                    sub_belt.append(box)
+
+    return answer
