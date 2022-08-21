8c5fd7fbeee9f9eae65a6e2637ee2746fdd058b7
Soarin Kim <soarin@madsq.net>
2022-08-21 15:25:30 +0900

    실전 모의고사 2회 3번 풀이

---
+++ 프로그래머스/실전모의고사2회/3.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+def solution(n, roads, sources, destination):
+    answer = []
+
+    graph = [[] for _ in range(n+1)]
+
+    for i in range(len(roads)):
+        a,b = roads[i]
+        graph[a].append(b)
+        graph[b].append(a)
+    
+
+    visit = [-1 for i in range(n+1)]
+
+    def bfs(start):
+        queue = deque()
+        queue.append(start)
+
+        
+        visit[start] = 0
+
+
+        while queue:
+            x = queue.popleft()
+
+            for i in graph[x]:
+                if visit[i] == -1:
+                    queue.append(i)
+                    visit[i] = visit[x]+1
+        
+        return visit[destination] 
+
+    bfs(destination)
+
+    for i in sources:
+        answer.append(visit[i])
+
+    return answer
