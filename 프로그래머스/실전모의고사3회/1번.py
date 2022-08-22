a9a3d02d28cb720c56f67478604adce047a75436
Soarin Kim <soarin@madsq.net>
2022-08-22 13:47:31 +0900

    실전 모의고사 3회 1번 풀이

---
+++ 프로그래머스/실전모의고사3회/1번.py
@@ -0,0 +1,14 @@
+def solution(a, b, n):
+    answer = 0
+
+    rest,new = divmod(n,a)
+    while True:
+        if n<a:
+            break
+        
+        rest,new = divmod(n,a)
+        rest = rest*b
+        answer += rest
+        n = rest+new
+
+    return answer
